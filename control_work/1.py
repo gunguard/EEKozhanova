3c55f8ae1237fe542c21e1d0e2f89b1401c1fec8
Gunguard <lizakozhanova@gmail.com>
2016-12-06 17:19:24 +0300

    change wrong file

--- control_work/1.py
+++ control_work/1.py
@@ -1,17 +1,18 @@
 # Программа должна открыть частотный словарь в кодировке UTF-8 и корректно вывести на экран только строчки с союзами.
 
-line = []
+line1 = []
 f = open('text.txt', 'r', encoding='utf-8')
 for i in f:
-    line = i.split(' | ')
-    if line[1].startswith('союз'):
-        print(line)
+    line1 = i.split(' | ')
+    if line1[1].startswith('союз'):
+        print(line1)
 f.close()
 
 # Программа должна распечатать через запятую все существительные женского рода единственного числа, а также вывести на экран сумму их ipm.
 line = []
 line2 = []
 line3=[]
+s=0
 f = open('text.txt', 'r', encoding='utf-8')
 for i in f:
     line = i.split(' | ')
@@ -24,5 +25,6 @@ for i in f:
     if line2[3] == 'муж' or line2[3] == 'ср' or line2[3] == 'общ':
         continue
     line3.append(line[0])
-    line3.append(line[2])
-print(line3)
+    n=float(line[2].strip('\n'))
+    s+=n
+print(', '.join(line3),s)
